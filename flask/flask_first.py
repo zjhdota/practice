9d151f92a8eac1221bd5fa8d00476535e1c1be00
zjhdota <zjhdota@outlook.com>
2018-01-09 09:21:28 +0800

    update
    update

---
+++ flask/flask_first.py
@@ -0,0 +1,26 @@
+# -*- encoding=utf-8 -*-
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+tasks = [
+    {
+        'id': 1,
+        'title': u'Buy groceries',
+        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol',
+        'done': False
+    },
+    {
+        'id': 2,
+        'title': u'Learn Python',
+        'description': u'Need to find a good Python tutorial on the web',
+        'done': False
+    }
+]
+
+@app.route('/123', methods=['GET'])
+def get_tasks():
+    return jsonify({'tasks': tasks})
+
+if __name__ == '__main__':
+    app.run()
---
+++ tensorflow/Linear_regression.py
@@ -0,0 +1,19 @@
+# -*- encoding=utf-8 -*-
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+rng = np.random()
+learning_rate = 0.01
+trainin_epochs = 2000
+display_step = 50
+
+
+
+
+
+
+
+
+
+
+
